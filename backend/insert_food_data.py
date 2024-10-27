50e2c672b9a69b58164e0f1e4a62be6e9ab1b5b1
Ashwin Ramesh Kannan <ashwinramesh.022@gmail.com>
2024-10-26 20:31:23 -0400

    fix startup

--- backend/insert_food_data.py
+++ backend/insert_food_data.py
@@ -1,4 +1,11 @@
+import sys
+import os
+
+# Add the root directory to sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from apps import App
+
 app = App()
 mongo = app.mongo
 
